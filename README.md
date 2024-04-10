11f3ac318ae296573feac9129b937e8582f7529a
u7287889 <u7287889@anu.edu.au>
2024-04-10 13:29:16 +1000

    initial commit - working gopher client implementation

---
+++ README.md
@@ -0,0 +1,59 @@
+# COMP3310 Assignment 2 - Indexing a Gopher
+
+## 2024 Semester 1 - U7287889 - Samman Palihapitiya
+
+> For this assignment, you need to write your own gopher client in C, Java or Python. The client will need to 'spider' or 'crawl' or 'index' a specified server, do some simple analysis and reporting of waht resources are there, as well as detect, report and deal with any issues with the server or its content.
+
+### RFC 1436: The Internet Gopher Protocol
+
+* Operates on port 70 by default
+* Requests to a Gopher server are simple strings - followed by a CR carriage return and LF line feed (i.e., `\r\n`)
+* Server's response can be directiories, text files or other - 0 for text files and 1 for directories
+
+More on this at section 3.8 'Item type characters' found [here](https://www.rfc-editor.org/rfc/rfc1436).
+
+---
+
+### Design Choices:
+
+In order to handle `Badly behaved pages` I had a collection of different approaches:
+
+`Firehose` - This resource sends an infinite stream of data as long as the socket is kept open, so in order to handle resources like this I've implemented a `max_size` restriction of 1MB to files. In the `download_file` function, we essentially keep requesting until the bytes received is less than `max_size`. There's no particular reason for choosing 1MB, it seemed large enough for a text file whilst still keeping my gopher-client functional.
+
+`Tarpit` - This is a resource that responds very slowly and is seemingly never ending. My approach to this was to implement the timeout attribute. The `socket.settimeout` attribute isn't effective for this as it only times out if there is no activity during the set period, however `tarpit` bypasses this as it does send responses - only very slowly. Therefore, I needed to set up my own `start_time` to handle such files to avoid lengthy hangs/infinite streams. I've currently set both the socket timeout and my own timeout variable to 10 seconds as this seemed like a fair time period to handle somewhat slow responses without compromising the responsiveness of my gopher-client.
+
+`Godot` - As per the file description, this file never comes - therefore, this was easily handled by the timeout setting of the socket.
+
+
+In order to exclude text/binary files that resulted in an error or some overflow, I've decided to return size as 0 in the event of a timeout or size being exceeded. By doing so, I am able to exclude them in the comparison done in `find_largest_and_smallest_files()` function.
+
+---
+
+
+
+### Known Limitations/Bugs:
+
+* hard limit of size of file set to 1MB, potentially losing valuable information
+* hard limit to socket timeout, again can lead to losing valuable contents
+* processing is explicit for directories, images, binary or text files - meaning other files are not handled as well
+* external references are filtered simply based on whether a line response has the string: "comp3310", although, it works for our assignment envrionment and the course's gopher server - nothing is stopping someone from adding an external reference for which the phrase 'comp3310' is present as part of it's path or url.
+* vice versa, we could potentially misidentify external references which are part of the course's gopher server but is located elsewhere -> leads back to how we define external references.
+* invalid references are not handled but simply dumped as lines -> hard to digest and trace
+* Since I've set the timeout to 10 seconds and max size to 1MB the crawling is slower than it could be - in order to improve this speed we can reduce the timeout buffer and the max size as needed.
+
+---
+
+
+
+### Wireshark Response:
+
+![1712630563240](image/README/1712630563240.png)
+
+Shown above is a snippet of the initial TCP three-way handshake, followed by the response from the course gopher server. I've expanded the gopher response received in line 63 as it is pretty digestible and evident of containing the initial root directory's content. I've also dumped the gopher packets from wireshark into an excel sheet (`Wiresharkcket_Dump.xlsx`)
+
+---
+
+### Requirements
+
+You will need to execute my python script in a powershell to be able to view the styled/coloured print output. Although it is not necessary, without it, you will see a lot ANSI characters that make no sense to you.
+Any terminal with ANSI enabled or working will be able to see the intended print style/colours.
---
+++ gopher_client.py
@@ -0,0 +1,448 @@
+# COMP3310 Assignment 2 - 2024 Semester 1 | Australian National University
+# @AUTHOR: U7287889 - Samman Palihapitiya
+
+import socket
+import os
+import time
+from typing import Deque, Dict, List, Set, Union, Optional, IO, Any
+import datetime
+from collections import deque
+import textwrap
+
+# ANSI codes for colours and styles
+HEADER: str = "\033[95m"
+OKBLUE: str = "\033[94m"
+OKCYAN: str = "\033[96m"
+OKGREEN: str = "\033[92m"
+WARNING: str = "\033[93m"
+FAIL: str = "\033[91m"
+ENDC: str = "\033[0m"
+BOLD: str = "\033[1m"
+UNDERLINE: str = "\033[4m"
+
+HOST: str = "comp3310.ddns.net"
+PORT: int = 70
+COURSE_TAG: str = "comp3310"
+
+# Item type characters - https://www.rfc-editor.org/rfc/rfc1436
+TXTFILE: str = "0"
+DIRECTORY: str = "1"
+CSO_PHONE: str = "2"
+ERROR: str = "3"
+BINHEX_MAC: str = "4"
+DOS_BINARY_ARCH: str = (
+    "5"  # must read until TCP closes - no period at EOF (.xxx extension?)
+)
+UNIX_UUEN: str = "6"
+INDEX_SEARCH: str = "7"
+TELNET: str = "8"
+BINARY: str = "9"  # must read until TCP closes - no period at EOF (.xxx extension?)
+REDUN_SERVER: str = "+"
+TN3270: str = "T"
+GIF: str = "g"
+IMG: str = "I"
+INFORMATION: str = "i"
+
+# doubly ended queue is efficient w popping items off
+DIR_TO_VISIT: Deque[str] = deque()
+# keep track of directories that have been searched
+DIRS_VISITED: Dict[str, str] = {}
+
+# will capture text, binary and image files from gopher server
+GOPHER_RESOURCES: List[Dict[str, Union[str, Optional[int], str]]] = []
+
+# error handling sets - helps deduplication
+INVAL_REFS: Set[str] = set()
+EX_REFS: Set[str] = set()
+
+# informational message in the server
+INFO_MESSAGE: List[str] = []
+
+FILE_SIZES: Dict[str, Optional[Dict[str, Union[str, int | None]]]] = {
+    "smallest_text_file": None,
+    "largest_text_file": None,
+    "smallest_binary_file": None,
+    "largest_binary_file": None,
+}
+
+
+def connect_to_server(host: str, port: int) -> socket.socket:
+    """
+    Establishes the TCP connection to the specified host and port.
+    Return an active socket object that is connected to server, however,
+    on fail, an exception is raised with details of the failure.
+    """
+
+    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        socket_obj.connect((host, port))
+    except Exception as e:
+        print(f"Error connecting to {host} on port {port}: {e}")
+        socket_obj.close()
+        raise
+    return socket_obj
+
+
+def send_request(selector: str, host: str, port: int) -> IO[Any]:
+    socket = connect_to_server(host, port)
+    # debugging print - comment out if necessary
+    print(
+        selector, "requested from", host, "on port", port, "-", datetime.datetime.now()
+    )
+
+    # create request with CRLF and send thru socket
+    request = selector + b"\r\n"
+    socket.send(request)
+
+    file = socket.makefile()
+    socket.close()
+    # response is returned as file
+    return file
+
+
+# given selector, item type, host and port, will return a string of the url to the resource
+# the returned url can be placed in the Floodgap url search bar for navigation
+def create_gopher_url(selector: str, item_type: str, host: str, port: int = 70) -> str:
+    return f"gopher://{host}:{port}/{item_type}{selector}"
+
+
+# used for the final print formatting to avoid overly long lines
+def print_wrapped(text: str, width: int):
+    wrapper = textwrap.TextWrapper(width=width)
+    wrapped_text = wrapper.fill(text)
+    print(wrapped_text)
+
+
+def handle_ex_refs(line: str) -> None:
+    """
+    This function processes external references and malformed lines returned by the gopher server. Primary
+    purpose in this case is to identify external references - i.e., pointers outside the comp3310 domain.
+    """
+    item_type = line[0]
+    rest_of_line = line[1:]
+    parts = rest_of_line.split("\t")
+
+    if len(parts) == 4:
+        _, _, host, port = parts
+        message = f"Host: {host}, Port: {port}"
+        EX_REFS.add(message)
+    else:
+        message = (
+            f"MALFORMED LINE/EXTERN REF ---> Item Type: {item_type} -- {rest_of_line}"
+        )
+        EX_REFS.add(message)
+
+    return None
+
+
+def handle_inval_refs(line: str) -> None:
+    # records invalid references from gopher server lines, item type 3 issues.
+    line = line[1:]  # exclude the type number
+    message = f"INVALID REF ---> {WARNING}{line}{ENDC}"
+    INVAL_REFS.add(message)
+    return None
+
+
+def parse_file(line: str, file_type: str) -> None:
+    """
+    Processes a line from the gopher menu that corresponds to a file (text/binary), downloads this file
+    and updates the list of resources for printing.
+    """
+
+    # extracting the file's directory from the line
+    start_index = line.find("/")
+    file_directory = line[start_index:]
+    end_index = file_directory.find(COURSE_TAG)
+    file_directory = file_directory[:end_index]
+
+    # download the file and get its size
+    size = download_file(file_directory, file_type)
+
+    # removes the trailing /t following selector/path
+    inval_index = file_directory.find("\t")
+    file_directory = file_directory[:inval_index]
+
+    # create the URL for accessing the file
+    url = create_gopher_url(file_directory, line[0], HOST, PORT)
+    file_resource = {"url": url, "size": size, "type": line[0]}
+
+    # append unique resources to the list
+    if file_resource not in GOPHER_RESOURCES:
+        GOPHER_RESOURCES.append(file_resource)
+
+    return None
+
+
+def parse_menu(file: IO[Any]) -> None:
+    """
+    Prases the content of a gopher directory listing, extracting information about direcotires, text and binary files.
+    Able to handle external references and error/invalid references. It updates global structures for directories to visit as well as
+    for resources found.
+
+    Each line from the given menu is analysed to determine its type - directory, text, binary etc. - appropriate action is taken based
+    on this. Directories get added to a queue for visting while text/binary files get downloaded for metadata analysis.
+    """
+
+    global DIR_TO_VISIT, DIRS_VISITED  # global lists get modified in here
+    for line in file:
+        line = line.strip()  # strip white space at front
+        item_type = line[0]  # gopher item type is the first char
+
+        # process directories
+        if item_type == DIRECTORY:
+
+            # exclude non-comp3310 related content -- bit tacky but seem to do the trick
+            if COURSE_TAG not in line:
+                handle_ex_refs(line)
+
+            start_index = line.find("/")
+            if (
+                start_index != -1
+            ):  # if fulfilled, likely to have found a path...continue processing
+                # note the end of directory path is denoted by the tab '\t'
+                end_index = line.find("\t", start_index)
+                # if the tab character is found, then we likely have complete path
+                if end_index != -1:
+                    dir_path = line[start_index:end_index]
+                else:
+                    dir_path = line[start_index:]
+
+                # add new path to directories to visit if it hasn't already been visited or is to be visited
+                if (
+                    dir_path
+                    and (dir_path not in DIRS_VISITED)
+                    and (dir_path not in DIR_TO_VISIT)
+                ):
+                    DIR_TO_VISIT.append(
+                        dir_path
+                    )  # new directory path added to visit list
+
+        # process text files
+        elif item_type == TXTFILE:
+            parse_file(line, "text")
+
+        # process binaries
+        elif item_type == BINARY:
+
+            file_type = "image" if "jpeg" in line else "binary"
+            parse_file(line, file_type)
+
+        # process error lines
+        elif item_type == ERROR:
+            handle_inval_refs(line)
+
+        else:
+            # handle information text
+            if item_type == INFORMATION:
+                info_text = line[1:].strip()
+                if not info_text.startswith("invalid"):
+                    message = f"{OKBLUE}Information:{ENDC} {info_text}"
+                    INFO_MESSAGE.append(message)
+    file.close()
+    return None
+
+
+def download_file(
+    selector: str, file_type: str, max_size: int = 1000000, timeout: int = 10
+) -> int:
+    """
+    Downloads a file from the gopher server. Able to handle requests for download of jpegs, binaries and text files.
+    Max file size limited to 1MB to prevent extremely large file downloads, timeout set to 10 seconds to avoid lengthy hangs.
+    """
+    print(
+        "Retrieving",
+        selector,
+        "from",
+        HOST,
+        "on port",
+        PORT,
+        "-",
+        datetime.datetime.now(),
+    )
+
+    # create request, socket/connection and set socket timeout
+    idx = selector.find("\\")
+    selector = selector[:idx]
+    request = str.encode(selector) + b"\r\n"
+    socket_obj = connect_to_server(HOST, PORT)
+    socket_obj.settimeout(10)
+    socket_obj.send(request)
+
+    # receive reponse of the file
+    response = b""
+    bytes_received = 0
+    start_time = time.time()
+
+    try:
+        while bytes_received < max_size:
+            if time.time() - start_time > timeout:
+                print(f"{WARNING}Timed out while retrieving {selector}.{ENDC}")
+                break
+
+            file_components = socket_obj.recv(2048)
+            if not file_components:
+                break
+
+            response += file_components
+            bytes_received += len(file_components)
+
+    except Exception as e:
+        print(f"{FAIL}Error while retrieving {selector}: {e}{ENDC}")
+        if bytes_received == 0:
+            print(f"{WARNING}Sorry, no data received for {selector}.{ENDC}")
+            return 0
+        return None
+    finally:
+        socket_obj.close()
+
+    # handles issues to do with OS specific termination, there was an error where
+    # empty.txt was showing size of 3 bytes - this was the solution.
+    if file_type == "text":
+        if response.endswith(b".\r\n"):
+            response = response[:-3]
+
+    file_extension_map = {"text": ".txt", "binary": ".dat", "image": ".jpeg"}
+    extension = file_extension_map.get(file_type, "_")
+
+    # file name capped to 50 characters
+    filename = f"{file_type}files{selector}"
+    if len(filename) > 50:
+        filename = filename[:50]
+
+    # only add file extension tag if its missing
+    if not filename.lower().endswith(extension):
+        filename += extension
+
+    filepath = os.path.join("gopher-downloads", filename)
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, "wb") as file:
+        file.write(response)
+
+    file_size = os.path.getsize(filepath)
+    return file_size if file_size < max_size else None
+
+
+def find_largest_and_smallest_files() -> None:
+    """
+    Identifies the largest and smallest text and binary files that were downloaded.
+    It excludes errored/timed out downloads by filtering sizes of None.
+    """
+    global FILE_SIZES
+    # float inf is positive infinite, therefore, any number we find will reset it
+    smallest_text_size = float("inf")
+    smallest_binary_size = float("inf")
+    largest_text_size = -1
+    largest_binary_size = -1
+
+    for resource in GOPHER_RESOURCES:
+        # ensures resource has valid size and not None, meaning it wasn't errored or timed out.
+        if "size" in resource and resource["size"] is not None:
+            if resource["type"] == TXTFILE:
+                # find smallest text file size
+                if resource["size"] < smallest_text_size:
+                    smallest_text_size = resource["size"]
+                    FILE_SIZES["smallest_text_file"] = resource
+                # find largest text file size
+                if resource["size"] > largest_text_size:
+                    largest_text_size = resource["size"]
+                    FILE_SIZES["largest_text_file"] = resource
+            elif resource["type"] == BINARY:
+                # find smallest binary file
+                if resource["size"] < smallest_binary_size:
+                    smallest_binary_size = resource["size"]
+                    FILE_SIZES["smallest_binary_file"] = resource
+                # find largest binary file
+                if resource["size"] > largest_binary_size:
+                    largest_binary_size = resource["size"]
+                    FILE_SIZES["largest_binary_file"] = resource
+    return None
+
+
+def web_crawler() -> None:
+    """
+    Crawls the gopher server, starting from the root directory, to discover and process resources.
+    """
+    global DIRS_VISITED
+
+    # iterate through all directories in gopher server until no more directories left to visit
+    while DIR_TO_VISIT:
+
+        current_dir = DIR_TO_VISIT.popleft()
+        if current_dir not in DIRS_VISITED:
+            # we know these are directories, so I've hardcoded the item type
+            url = create_gopher_url(current_dir, "1", HOST, PORT)
+            DIRS_VISITED[current_dir] = url
+
+            selector = str.encode(current_dir)
+            # grab page content
+            web_page = send_request(selector, HOST, PORT)
+            # perform processing - image, text, binary, directory
+            parse_menu(web_page)
+    return None
+
+
+def main() -> None:
+    # start from root directory - b''
+    web_page = send_request(b"", HOST, PORT)
+    parse_menu(web_page)
+    # crawl gopher server
+    web_crawler()
+
+    find_largest_and_smallest_files()
+    return None
+
+
+if __name__ == "__main__":
+    main()
+
+    text_files = [
+        resource for resource in GOPHER_RESOURCES if resource["type"] == TXTFILE
+    ]
+    binary_files = [
+        resource for resource in GOPHER_RESOURCES if resource["type"] == BINARY
+    ]
+
+    print(BOLD + "=" * 130)
+    print(BOLD + UNDERLINE + "Directory and File Count Information:" + ENDC)
+    print(f"{OKGREEN}Number of Directories:{ENDC} {FAIL}{len(DIRS_VISITED)}{ENDC}")
+
+    print(BOLD + "-" * 130)
+    print(OKGREEN + f"Number of External References:{ENDC} {FAIL}{len(EX_REFS)}" + ENDC)
+    for i, ref in enumerate(sorted(EX_REFS), start=1):
+        print(f"\t{i}) {OKCYAN}{ref}{ENDC}")
+
+    print(BOLD + "-" * 130)
+    print(
+        OKGREEN
+        + f"Number of Invalid References (Error Types):{ENDC} {FAIL}{len(INVAL_REFS)}"
+        + ENDC
+    )
+    for j, ref in enumerate(sorted(INVAL_REFS), start=1):
+        print(f"\t{j}) {ref}")
+
+    print(BOLD + "-" * 130)
+    print(OKGREEN + f"Number of Text Files:{ENDC} {FAIL}{len(text_files)}" + ENDC)
+    for i, file in enumerate(text_files, start=1):
+        print_wrapped(f"\t{i}) {OKCYAN}{file['url']}{ENDC}", 120)
+
+    print(BOLD + "-" * 130)
+    print(OKGREEN + f"Number of Binary Files:{ENDC} {FAIL}{len(binary_files)}" + ENDC)
+    for i, file in enumerate(binary_files, start=1):
+        print(f"\t{i}) {OKCYAN}{file['url']}{ENDC}")
+
+    print(BOLD + "=" * 130)
+    print(BOLD + UNDERLINE + "File Size Information:" + ENDC)
+    for category, file_info in FILE_SIZES.items():
+        if file_info:
+            print(
+                f"{category.replace('_', ' ').title()}: {OKCYAN}{file_info['url']}{ENDC} {OKGREEN}(Size: {file_info['size']} bytes){ENDC}"
+            )
+        else:
+            print(f"{category.replace('_', ' ').title()}: None")
+
+    print(BOLD + "=" * 130)
+
+    print(BOLD + UNDERLINE + "Informational Messages:" + ENDC)
+    for message in INFO_MESSAGE:
+        print(message)
+    print(BOLD + "=" * 130)
